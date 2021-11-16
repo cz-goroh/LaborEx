4ac4863fe4f3a7647843f34f469b3724a107761c
Segura <segura-deluna@mail.ru>
2021-11-17 02:06:29 +0300

    last update

--- raw_layout/Segura/src/js/script.js
+++ raw_layout/Segura/src/js/script.js
@@ -24,6 +24,28 @@ document.addEventListener('DOMContentLoaded', () => {
     }
   };
 
+  // * Burger-modal Tabs ==========================================
+  // * ============================================================
+  document.querySelectorAll('.tabs-triggers__item').forEach((item) => {
+    item.addEventListener('click', (e) => {
+      e.preventDefault();
+      const id = e.target.getAttribute('href').replace('#', '');
+
+      document.querySelectorAll('.tabs-triggers__item').forEach((child) => {
+        child.classList.remove('tabs-triggers__item--active');
+      });
+
+      document.querySelectorAll('.tabs-content__item').forEach((child) => {
+        child.classList.remove('tabs-content__item--active');
+      });
+
+      item.classList.add('tabs-triggers__item--active');
+      document.getElementById(id).classList.add('tabs-content__item--active');
+    });
+  });
+
+  document.querySelector('.tabs-triggers__item').click();
+
   // * Open-Close modals ===========================================
   // * =============================================================
 
